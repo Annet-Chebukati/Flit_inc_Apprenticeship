d439144f00507d01795da457b4b2bb9154b541f2
Annet-Chebukati <119885320+Annet-Chebukati@users.noreply.github.com>
2023-12-23 13:52:07 +0300

    Update Sentiment_Analyser.py

--- DataScienceandAIprojects/Sentiment_Analysis_for_Product_Reviews/Sentiment_Analyser.py
+++ DataScienceandAIprojects/Sentiment_Analysis_for_Product_Reviews/Sentiment_Analyser.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 
 # Import necessary libraries
 import streamlit as st
@@ -32,9 +27,9 @@ warnings.filterwarnings("ignore")
 plt.style.use('ggplot')
 
 # Load the data
-@st.cache
+@st.cache_data
 def load_data():
-    data_path = "C:\\ANITA CHEBUKATI\\FLIT\\Sentiment Analysis for Product Review\\Amazon Product Review.txt"
+    data_path = "https://github.com/Annet-Chebukati/Flit_inc_Apprenticeship/blob/master/DataScienceandAIprojects/Sentiment_Analysis_for_Product_Reviews/Amazon%20Product%20Review.txt"
     amz_df = pd.read_csv(data_path)
     return amz_df
 
@@ -79,7 +74,11 @@ fit_model = clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
 # Streamlit code to display results
-st.title('Sentiment Analysis Model Results')
+st.title('Sentiment Analysis for Product Reviews')
+st.write('This app uses a Random Forest Classifier to classify the sentiment of Amazon Product Reviews.')
+st.write(
+    'To explore the Amazon Product Review data, please check out my [Github](https://github.com/Annet-Chebukati/Flit_inc_Apprenticeship/blob/master/DataScienceandAIprojects/Sentiment_Analysis_for_Product_Reviews/Amazon%20Product%20Review.txt). To see my source code, have a look at my [GitHub repo](https://github.com/Annet-Chebukati/Flit_inc_Apprenticeship/tree/master/DataScienceandAIprojects/Sentiment_Analysis_for_Product_Reviews).')
+st.write('*Note: it will take up a few seconds to run the app.*')
 st.write('Training accuracy:', fit_model.score(X_train, y_train))
 st.write('Test accuracy:', fit_model.score(X_test, y_test))
 st.write('AUC-ROC:', roc_auc_score(y_test, y_pred))
@@ -88,3 +87,20 @@ st.text(classification_report(y_test, y_pred))
 st.text('Confusion Matrix:')
 st.text(confusion_matrix(y_test, y_pred))
 
+# Create a form for user input
+form = st.form(key='sentiment-form')
+user_input = form.text_area('Enter your review')
+submit = form.form_submit_button('Submit')
+# Process the user input when the form is submitted
+if submit:
+    # Vectorize the user's input
+    user_input_vectorized = vectorizer.transform([user_input])
+
+    # Predict the sentiment of the user's input
+    prediction = clf.predict(user_input_vectorized)
+
+    # Display the prediction with a matching emoji
+    if prediction == 0:
+        st.error('Negative sentiment 😞')
+    else:
+        st.success('Positive sentiment 😄')
